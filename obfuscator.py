266d94447dfc05a68765de6c6707a8d4c81a3ae5
Tamiyyah <tshafiq50@stuy.edu>
2025-05-28 06:21:30 -0400

    tested on one of the old decodes, added a line to account for no spacing

--- obfuscator.py
+++ obfuscator.py
@@ -8,6 +8,7 @@ def findVarNames(file):
     file = open(file, 'r')  # opens file and reads it
     code = file.read()
     varNames = re.findall(r'[A-Za-z]+ =', code) # finds everything preceding a =
+    varNames += re.findall(r'[A-Za-z]+=', code) # finds everything preceding a = w/o space
     file.close()
 
     for i in range(len(varNames)): # gets rid of the = in my list
@@ -17,4 +18,5 @@ def findVarNames(file):
     return varNames
 
 
-findVarNames("code.txt")
+findVarNames("testingCodeFiles/crack.py")
+
