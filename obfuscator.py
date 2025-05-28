3d74f4380666e459cb3edf4a05d6c62fb0aefbdc
Haocheng Huang <haocheng.huang07@gmail.com>
2025-05-28 14:45:36 -0400

    added dictionary outputs to file

--- obfuscator.py
+++ obfuscator.py
@@ -1,40 +1,52 @@
 import re, secrets, string, random
 
+
 def findNames(file):
-    file = open(file, 'r')  # opens file and reads it
-    code = file.read()
-    names = re.findall(r'[A-Za-z]+ =', code) # finds variable names preceding a =
-    names += re.findall(r'[A-Za-z]+=', code) # finds variable names preceding a = w/o space
-    names += re.findall(r'def [A-Za-z]+', code) # find function names
-    file.close()
+   file = open(file, 'r')  # opens file and reads it
+   code = file.read()
+   names = re.findall(r'[A-Za-z]+ =', code) # finds variable names preceding a =
+   names += re.findall(r'[A-Za-z]+=', code) # finds variable names preceding a = w/o space
+   names += re.findall(r'def [A-Za-z]+', code) # find function names
+   file.close()
 
-    for i in range(len(names)): # gets rid of the = in my list
-        names[i] = names[i].rstrip(" =")
-        names[i] = names[i].lstrip("def ")
 
-    names = list(set(names)) # unique names
-    return names
+   for i in range(len(names)): # gets rid of the = in my list
+       names[i] = names[i].rstrip(" =")
+       names[i] = names[i].lstrip("def ")
 
-def replaceNames(file):
-    names = findNames(file)
 
-    map = {}
+   names = list(set(names)) # unique names
+   return names
 
-    file = open(file, 'r')  # opens file and reads it
-    code = file.read()
-    file.close()
 
-    for i in names:
-        length = random.randint(3, 10) # random length for the new variable name
-        newName = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length)) # random string
-        code = code.replace(i, newName)
-        map[i] = newName
-    
-    print(code)
-    print(map)
-    return code
+def replaceNames(file, output):
+   names = findNames(file)
 
-replaceNames("testingCodeFiles/crack.py")
 
+   map = {}
+
+
+   file = open(file, 'r')  # opens file and reads it
+   code = file.read()
+   file.close()
+
+
+   for i in names:
+       length = random.randint(3, 10) # random length for the new variable name
+       newName = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length)) # random string
+       code = code.replace(i, newName)
+       map[i] = newName
+
+
+   output = open(output, 'w')
+   output.write(str(map))
+  
+   print(code)
+   print(map)
+   return code
+
+
+replaceNames("testingCodeFiles/crack.py", "output.txt")
 # TODO current concerns: want to make sure that if i have a variable name reused in diff defs, it isn't an issue. i don't think it should be
 # TODO test mapping file
+
