c9db7bc626ea7ef859ee2cb5831da55dad0b5f04
Tamiyyah <tshafiq50@stuy.edu>
2025-05-30 14:49:12 -0400

    reverted

--- obfuscator.py
+++ obfuscator.py
@@ -1,11 +1,5 @@
 import re, secrets, string, random, encode
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> b602a87617b3cc3d52aec4a829ffea1314123499
 def findNames(file):
    file = open(file, 'r')  # opens file and reads it
    code = file.read()
@@ -14,42 +8,21 @@ def findNames(file):
    names += re.findall(r'def [A-Za-z]+', code) # find function names
    file.close()
 
-
-
-
    for i in range(len(names)): # gets rid of the = in my list
        names[i] = names[i].rstrip(" =")
        names[i] = names[i].lstrip("def ")
 
-
-
-
    names = list(set(names)) # unique names
    return names
 
-
-def findNewline(file):
-   file = open(file,'r')
-   code = file.read()
-   newlines = re.findall(r'[\r\n]+', code)
-   return newlines
-
-
 def replaceNames(file, output):
    names = findNames(file)
-   newlines = findNewline(file)
-
 
    map = {}
-
-
-
 
    file = open(file, 'r')  # opens file and reads it
    code = file.read()
    file.close()
-
-
 
 
    for i in names:
@@ -58,9 +31,6 @@ def replaceNames(file, output):
        newName = encode.encode(i, "hao")
        code = code.replace(i, newName)
        map[i] = newName
-   for i in newlines:
-       code = code.replace(i, "")
-
 
    output = open(output, 'w')
    output.write(str(map))
@@ -69,14 +39,10 @@ def replaceNames(file, output):
    print(map)
    return code
 
-
-
+def findSpaces(file):
+   file = open(file, 'r')
+   code = file.read()
 
 replaceNames("testingCodeFiles/crack.py", "output.txt")
 # TODO current concerns: want to make sure that if i have a variable name reused in diff defs, it isn't an issue. i don't think it should be
 # TODO test mapping file
-
-
-
-
-
