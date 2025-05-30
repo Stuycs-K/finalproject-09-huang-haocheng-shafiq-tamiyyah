b602a87617b3cc3d52aec4a829ffea1314123499
Haocheng Huang <haocheng.huang07@gmail.com>
2025-05-30 14:37:48 -0400

    remove newlines

--- obfuscator.py
+++ obfuscator.py
@@ -1,6 +1,8 @@
 import re, secrets, string, random, encode
 
 
+
+
 def findNames(file):
    file = open(file, 'r')  # opens file and reads it
    code = file.read()
@@ -10,25 +12,41 @@ def findNames(file):
    file.close()
 
 
+
+
    for i in range(len(names)): # gets rid of the = in my list
        names[i] = names[i].rstrip(" =")
        names[i] = names[i].lstrip("def ")
+
+
 
 
    names = list(set(names)) # unique names
    return names
 
 
+def findNewline(file):
+   file = open(file,'r')
+   code = file.read()
+   newlines = re.findall(r'[\r\n]+', code)
+   return newlines
+
+
 def replaceNames(file, output):
    names = findNames(file)
+   newlines = findNewline(file)
 
 
    map = {}
 
 
+
+
    file = open(file, 'r')  # opens file and reads it
    code = file.read()
    file.close()
+
+
 
 
    for i in names:
@@ -37,6 +55,8 @@ def replaceNames(file, output):
        newName = encode.encode(i, "hao")
        code = code.replace(i, newName)
        map[i] = newName
+   for i in newlines:
+       code = code.replace(i, "")
 
 
    output = open(output, 'w')
@@ -47,7 +67,13 @@ def replaceNames(file, output):
    return code
 
 
+
+
 replaceNames("testingCodeFiles/crack.py", "output.txt")
 # TODO current concerns: want to make sure that if i have a variable name reused in diff defs, it isn't an issue. i don't think it should be
 # TODO test mapping file
+
+
+
+
 
