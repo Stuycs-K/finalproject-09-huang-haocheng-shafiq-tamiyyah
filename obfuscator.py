d94a744f066b70d616c76551a47f925db9d58df1
Haocheng Huang <haocheng.huang07@gmail.com>
2025-06-02 14:11:41 -0400

    implmented deadcode function

--- obfuscator.py
+++ obfuscator.py
@@ -1,99 +1,137 @@
 import re, secrets, string, random, encode, sys
 
+
 def findNames(file):
-   file = open(file, 'r')  # opens file and reads it
-   code = file.read()
-   names = re.findall(r'[A-Za-z_][A-Za-z0-9_]* =', code) # finds variable names preceding a =
-   names += re.findall(r'[A-Za-z_][A-Za-z0-9_]*=', code) # finds variable names preceding a = w/o space
-   names += re.findall(r'def [A-Za-z_][A-Za-z0-9_]*', code) # find function names
-   file.close()
+  file = open(file, 'r')  # opens file and reads it
+  code = file.read()
+  names = re.findall(r'[A-Za-z_][A-Za-z0-9_]* =', code) # finds variable names preceding a =
+  names += re.findall(r'[A-Za-z_][A-Za-z0-9_]*=', code) # finds variable names preceding a = w/o space
+  names += re.findall(r'def [A-Za-z_][A-Za-z0-9_]*', code) # find function names
+  file.close()
 
-   for i in range(len(names)): # gets rid of the = in my list
-      names[i] = names[i].rstrip(" =")
-      if names[i].startswith("def "):
-         names[i] = names[i].lstrip("def ")
 
-   names = list(set(names)) # unique names
-   #print(names)
-   return names
+  for i in range(len(names)): # gets rid of the = in my list
+     names[i] = names[i].rstrip(" =")
+     if names[i].startswith("def "):
+        names[i] = names[i].lstrip("def ")
+
+
+  names = list(set(names)) # unique names
+  #print(names)
+  return names
+
 
 def replaceNames(file, output, version, key=None): # -v is vigenere, -m is random mapping, -s is random seed
-   names = findNames(file)
-   
-   output = open(output, 'w')
+  names = findNames(file)
+ 
+  output = open(output, 'w')
 
-   if version == "-m":
-      print("Random mapping mode.")
-      map = {}
-   elif version == "-v":
-      print("Vigenere mode.")
-      if key is None or key == "None" or key == "":
-         raise ValueError("Vigenère mode requires a key.")
-      map = key
-   elif version == "-s":
-      print("Random seed mode.")
-      if key is None or key == "None" or key == "":
-         raise ValueError("Seed mode requires a key.")
-      map = {}
-      output.write("key: " + key)
-   else:
-      raise ValueError("Please enter valid key.")
 
-   file = open(file, 'r')  # opens file and reads it
-   code = file.read()
-   file.close()
+  if version == "-m":
+     print("Random mapping mode.")
+     map = {}
+  elif version == "-v":
+     print("Vigenere mode.")
+     if key is None or key == "None" or key == "":
+        raise ValueError("Vigenère mode requires a key.")
+     map = key
+  elif version == "-s":
+     print("Random seed mode.")
+     if key is None or key == "None" or key == "":
+        raise ValueError("Seed mode requires a key.")
+     map = {}
+     output.write("key: " + key)
+  else:
+     raise ValueError("Please enter valid key.")
 
-   for i in names:
-      if version == "-m":
-         length = random.randint(3, 10) # random length for the new variable name
-         newName = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length)) # random string
-         map[i] = newName
-         code = code.replace(i, newName)
-      if version == "-v":
-         newName = encode.encode(i, key)
-         code = code.replace(i, newName)
-      if version == "-s":
-         random.seed(i + key)
-         length = random.randint(3, 10) # random length for the new variable name
-         newName = ''.join(random.choice(string.ascii_letters) for _ in range(4))
-         code = code.replace(i, newName)
-         #print(newName)
-         map[i] = length
 
-   
-   output.write(str(map))
-  
+  file = open(file, 'r')  # opens file and reads it
+  code = file.read()
+  file.close()
+
+
+  for i in names:
+     if version == "-m":
+        length = random.randint(3, 10) # random length for the new variable name
+        newName = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length)) # random string
+        map[i] = newName
+        code = code.replace(i, newName)
+     if version == "-v":
+        newName = encode.encode(i, key)
+        code = code.replace(i, newName)
+     if version == "-s":
+        random.seed(i + key)
+        length = random.randint(3, 10) # random length for the new variable name
+        newName = ''.join(random.choice(string.ascii_letters) for _ in range(4))
+        code = code.replace(i, newName)
+        #print(newName)
+        map[i] = length
+
+
+ 
+  output.write(str(map))
    print(code)
-   #print(output)
-   print(map)
-   return code
+  #print(output)
+  print(map)
+  return code
+
 
 def findSpaces(file):
-   file = open(file, 'r')
-   code = file.read()
+  file = open(file, 'r')
+  code = file.read()
 
-   symbols = ["=", "%", "<", ">", "≤", "≥", "=", ","]
 
-   for i in symbols:
-      code = code.replace(" " + i + " ", i)
-      code = code.replace(" " + i, i)
-      code = code.replace(i + " ", i)
+  symbols = ["=", "%", "<", ">", "≤", "≥", "=", ","]
 
-   print(code)
-   return code
+
+  for i in symbols:
+     code = code.replace(" " + i + " ", i)
+     code = code.replace(" " + i, i)
+     code = code.replace(i + " ", i)
+
+
+  print(code)
+  file.close()
+  return code
+
 
 def findNewLines(file):
-   file = open(file, 'r')
-   code = file.read()
-   code = re.sub(r'^\s*\n', '', code, flags=re.MULTILINE)
-   #lines = re.findall(r'[A-Za-z_][A-Za-z0-9_]* =', code) # finds variable names preceding a =
-   print(code)
-   return code
+  file = open(file, 'r')
+  code = file.read()
+  file.close()
+  code = re.sub(r'^\s*\n', '', code, flags=re.MULTILINE)
+  #lines = re.findall(r'[A-Za-z_][A-Za-z0-9_]* =', code) # finds variable names preceding a =
+  print(code)
+  return code
+
+
+def deadCode(file):
+  file = open(file, 'r')
+  code = file.read()
+  file.close()
+
+
+  length = random.randint(3, 10) # random length for the new variable name
+  newName = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length)) # random string
+
+
+  deadRet = "if " + str(random.randint(3, 10)) + " == " + "0" + ":" + "\n\t\t" + "return " + newName
+
+
+  code = re.sub(r'(return\s+\w+)', lambda match: f'{match.group(0)}\n\t{deadRet}', code)
+
+
+  print(code)
+  return code
+
 
 #findNames("testingCodeFiles/crack.py")
 #findSpaces("testingCodeFiles/crack.py")
-findNewLines("testingCodeFiles/crack.py")
+#findNewLines("testingCodeFiles/crack.py")
+deadCode("testingCodeFiles/crack.py")
 #replaceNames("testingCodeFiles/crack.py", "output.txt", sys.argv[1], sys.argv[2])
 # TODO current concerns: want to make sure that if i have a variable name reused in diff defs, it isn't an issue. i don't think it should be
 # TODO test mapping file
 # TODO change () variables too
+
+
