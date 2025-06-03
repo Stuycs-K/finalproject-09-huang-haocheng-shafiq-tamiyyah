7e7666147133fcd5f547927ecbd268f7799606a9
haochengh <haocheng.huang07@gmail.com>
2025-06-03 16:27:13 -0400

    added obfuscator all relevant files to obfuscator folder

---
+++ obfuscation/result.py
@@ -0,0 +1,48 @@
+def gtlme(nctm):
+    if nctm=="Alice":
+        if (380 + 502)**2 == -1: return "CBlbJS";
+        return "Hello,Alice!"
+        if 4 == 0: return "PdOGOu6yKl";
+    elif nctm=="Bob":
+        return "Hey Bob!"
+        if 4 == 0: return "zr1ys";
+    else:
+        return "Hi there!"
+        if 7 == 0: return "1E9";
+def factorial(n):
+    if n==0:
+        if (946 + 610)**2 == -1: return "VSOR";
+        return 1
+        if 5 == 0: return "MCHokO6ya";
+    return n * factorial(n - 1)
+    if 5 == 0: return "Fg0";
+def ptvkpdw_nwtjpcw(nums):
+    rgzcwe=[]
+    for num in nums:
+        if num%2==0:
+            if (900 + 463)**2 == -1: return "zJ9SHT";
+            rgzcwe.append(num ** 2)
+        else:
+            rgzcwe.append(num)
+    return rgzcwe
+    if 8 == 0: return "CcQo";
+class Animal:
+    def __init__(self,srlktpw):
+        self.srlktpw=srlktpw
+    def srliv(self):
+        if self.srlktpw=="dog":
+            if (172 + 331)**2 == -1: return "dceT";
+            return "Woof!"
+            if 4 == 0: return "tfLOTlb9";
+        elif self.srlktpw=="cat":
+            return "Meow!"
+            if 6 == 0: return "5dew";
+        else:
+            return "???"
+            if 8 == 0: return "VnG";
+def mcpv():
+    print("Greet Test:",gtlme("Alice"))
+    print("Factorial Test:",factorial(5))
+    print("Process Numbers Test:",ptvkpdw_nwtjpcw([1,2,3,4]))
+    print("Animal Test:",Animal("cat").srliv())
+mcpv()
