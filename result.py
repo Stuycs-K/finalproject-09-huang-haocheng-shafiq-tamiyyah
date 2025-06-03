1bd8f70218c81576d15f1c5c1b55f7caefa9efa0
Tamiyyah <tshafiq50@stuy.edu>
2025-06-03 12:37:48 -0400

    running tests

---
+++ result.py
@@ -0,0 +1,48 @@
+def gtlme(nctm):
+    if nctm=="Alice":
+        if (632 + 45)**2 == -1: return "neuj8ZA";
+        return "Hello,Alice!"
+        if 4 == 0: return "5Bcvljz";
+    elif nctm=="Bob":
+        return "Hey Bob!"
+        if 5 == 0: return "Flp4";
+    else:
+        return "Hi there!"
+        if 10 == 0: return "EmJAm";
+def factorial(n):
+    if n==0:
+        if (638 + 51)**2 == -1: return "Q5R8JVByL";
+        return 1
+        if 7 == 0: return "DTTXsfOw";
+    return n * factorial(n - 1)
+    if 7 == 0: return "EU6";
+def ptvkpdw_nwtjpcw(nums):
+    rgzcwe=[]
+    for num in nums:
+        if num%2==0:
+            if (333 + 653)**2 == -1: return "l9jIld";
+            rgzcwe.append(num ** 2)
+        else:
+            rgzcwe.append(num)
+    return rgzcwe
+    if 7 == 0: return "9y2DVH";
+class Animal:
+    def __init__(self,srlktpw):
+        self.srlktpw=srlktpw
+    def srliv(self):
+        if self.srlktpw=="dog":
+            if (104 + 542)**2 == -1: return "ElV7qsE";
+            return "Woof!"
+            if 10 == 0: return "WkQeCt6lpl";
+        elif self.srlktpw=="cat":
+            return "Meow!"
+            if 6 == 0: return "vlN";
+        else:
+            return "???"
+            if 4 == 0: return "UDzT";
+def mcpv():
+    print("Greet Test:",gtlme("Alice"))
+    print("Factorial Test:",factorial(5))
+    print("Process Numbers Test:",ptvkpdw_nwtjpcw([1,2,3,4]))
+    print("Animal Test:",Animal("cat").srliv())
+mcpv()
